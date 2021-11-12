1b49ebf342c0c90b250d330e8fbcb3f2b0360303
andrewbay <daddnbay@gmail.com>
2021-11-12 10:14:45 -0800

    commit tree d912b17b7a2cead02810eb8049bcb360e2a368c9

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function scuberGreetingForFeet(feet){
   // Write your code here!
-  if( distance <= 400){
+  if( feet <= 400){
     return 'This one is on me!'
   }
-  else if (distance > 2000 && distance < 2500){
+  else if (feet > 2000 && feet < 2500){
     return 'I will gladly take your thirty bucks.'
   }
   else {
